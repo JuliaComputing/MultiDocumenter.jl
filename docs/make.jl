77e189e68814b7abdceed8a75b5b2cb701208df3
Sebastian Pfitzner <pfitzseb@gmail.com>
2022-10-10 16:30:10 +0200

    Add deploy step to CI

---
+++ docs/make.jl
@@ -0,0 +1,58 @@
+using MultiDocumenter
+
+clonedir = mktempdir()
+
+docs = [
+    MultiDocumenter.DropdownNav("Debugging", [
+        MultiDocumenter.MultiDocRef(
+            upstream = joinpath(clonedir, "Infiltrator"),
+            path = "inf",
+            name = "Infiltrator",
+            giturl = "https://github.com/JuliaDebug/Infiltrator.jl.git",
+        ),
+        MultiDocumenter.MultiDocRef(
+            upstream = joinpath(clonedir, "JuliaInterpreter"),
+            path = "debug",
+            name = "JuliaInterpreter",
+            giturl = "https://github.com/JuliaDebug/JuliaInterpreter.jl.git",
+        ),
+    ]),
+    MultiDocumenter.MultiDocRef(
+        upstream = joinpath(clonedir, "DataSets"),
+        path = "data",
+        name = "DataSets",
+        giturl = "https://github.com/JuliaComputing/DataSets.jl.git",
+        # or use ssh instead for private repos:
+        # giturl = "git@github.com:JuliaComputing/DataSets.jl.git",
+    ),
+]
+
+outpath = mktempdir()
+
+MultiDocumenter.make(
+    outpath,
+    docs;
+    search_engine = MultiDocumenter.SearchConfig(
+        index_versions = ["stable"],
+        engine = MultiDocumenter.FlexSearch
+    )
+)
+
+gitroot = normpath(joinpath(@__DIR__, ".."))
+try
+    run(`git checkout gh-pages`)
+catch err
+    # branch probably doesn't exist
+    run(`git checkout -b gh-pages`)
+end
+for file in readdir(gitroot; join = true)
+    endswith(file, ".git") && continue
+    rm(file; force = true, recursive = true)
+end
+for file in readdir(outpath)
+    cp(joinpath(outpath, file), joinpath(gitroot, file))
+end
+run(`git add .`)
+run(`git commit -m 'Aggregate documentation'`)
+run(`git push`)
+run(`git checkout main`)
