abcdc2eecb78c996483007e17c4c6dcd182698f0
Morten Piibeleht <morten.piibeleht@juliahub.com>
2023-06-06 21:05:46 +1200

    Guard docs/make.jl against accidental deployment

--- docs/make.jl
+++ docs/make.jl
@@ -81,33 +81,39 @@ MultiDocumenter.make(
     rootpath = "/MultiDocumenter.jl/",
 )
 
-gitroot = normpath(joinpath(@__DIR__, ".."))
-run(`git pull`)
-outbranch = "gh-pages"
-has_outbranch = true
-if !success(`git checkout $outbranch`)
-    has_outbranch = false
-    if !success(`git switch --orphan $outbranch`)
-        @error "Cannot create new orphaned branch $outbranch."
-        exit(1)
+if "deploy" in ARGS
+    @warn "Deploying to GitHub" ARGS
+    gitroot = normpath(joinpath(@__DIR__, ".."))
+    run(`git pull`)
+    outbranch = "gh-pages"
+    has_outbranch = true
+    if !success(`git checkout $outbranch`)
+        has_outbranch = false
+        if !success(`git switch --orphan $outbranch`)
+            @error "Cannot create new orphaned branch $outbranch."
+            exit(1)
+        end
     end
-end
-for file in readdir(gitroot; join = true)
-    endswith(file, ".git") && continue
-    rm(file; force = true, recursive = true)
-end
-for file in readdir(outpath)
-    cp(joinpath(outpath, file), joinpath(gitroot, file))
-end
-run(`git add .`)
-if success(`git commit -m 'Aggregate documentation'`)
-    @info "Pushing updated documentation."
-    if has_outbranch
-        run(`git push`)
+    for file in readdir(gitroot; join = true)
+        endswith(file, ".git") && continue
+        rm(file; force = true, recursive = true)
+    end
+    for file in readdir(outpath)
+        cp(joinpath(outpath, file), joinpath(gitroot, file))
+    end
+    run(`git add .`)
+    if success(`git commit -m 'Aggregate documentation'`)
+        @info "Pushing updated documentation."
+        if has_outbranch
+            run(`git push`)
+        else
+            run(`git push -u origin $outbranch`)
+        end
+        run(`git checkout main`)
     else
-        run(`git push -u origin $outbranch`)
+        @info "No changes to aggregated documentation."
     end
-    run(`git checkout main`)
 else
-    @info "No changes to aggregated documentation."
+    @info "Skipping deployment, 'deploy' not passed. Generated files in docs/out." ARGS
+    cp(outpath, joinpath(@__DIR__, "out"))
 end
